379822fbd0a4c275401fe239937873fdee1d7b55
Jesus Pereira <jesus.viccken@gmail.com>
2026-09-21 11:03:15 -0300

    feat: categorizar viseiras na Linha Protecao e atualizar navegacao e banners do site

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CustomCursor } from './components/CustomCursor';
 import { HeaderNavbar } from './components/HeaderNavbar';
 import { HeroSection } from './components/HeroSection';
 import { BrandManifesto } from './components/BrandManifesto';
+import { BrandLinesOverview } from './components/BrandLinesOverview';
 import { ViseirasShowcase } from './components/ViseirasShowcase';
 import { DropBicolorFitnessShowcase } from './components/DropBicolorFitnessShowcase';
 import { TreinosQueridinhosShowcase } from './components/TreinosQueridinhosShowcase';
@@ -64,6 +65,7 @@ export const App: React.FC = () => {
       <main className="w-full overflow-x-hidden">
         <HeroSection onSubscribeSuccess={() => {}} />
         <BrandManifesto />
+        <BrandLinesOverview />
         <ViseirasShowcase />
         <DropBicolorFitnessShowcase />
         <TreinosQueridinhosShowcase />
